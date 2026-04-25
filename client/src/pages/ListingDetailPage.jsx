193eb498080c6722277d65ff901e16ac3b4bd819
Manasvini <tammineedim@gmail.com>
2026-04-24 18:40:07 -0700

    Add PhotoGallery component to ListingDetailPage

--- client/src/pages/ListingDetailPage.jsx
+++ client/src/pages/ListingDetailPage.jsx
@@ -1,3 +1,14 @@
+import PhotoGallery from "../components/PhotoGallery";
+
 export default function ListingDetailPage() {
-  return <h1>Listing Detail</h1>
+  const listing = {
+    photos: [],
+  };
+
+  return (
+    <div>
+      <h1>Listing Detail</h1>
+      <PhotoGallery photos={listing.photos} />
+    </div>
+  );
 }
